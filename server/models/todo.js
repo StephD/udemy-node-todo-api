54bb5c96cb0aa06e72712278acd30d766129c9c4
depsteph <dep.step@outlook.com>
2018-11-26 15:31:48 +0800

    config

--- server/models/todo.js
+++ server/models/todo.js
@@ -8,6 +8,7 @@ const Todo = mongoose.model(
 		text: { type: String, required: true, minlength: 1, trim: true },
 		completed: { type: Boolean, default: false },
 		completedAt: { type: Number, default: null },
+		_creator: { type: mongoose.Schema.Types.ObjectId, required: true },
 	},
 	'Todos',
 )
--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+require('./config/config')
+
 const express = require('express')
 const app = express()
 const bp = require('body-parser')
@@ -10,7 +12,6 @@ const { Todo } = require('./models/todo')
 const { User } = require('./models/user')
 const { authenticate } = require('./middleware/authenticate')
 
-require('./config/config')
 const port = process.env.PORT
 
 app.use(bp.json())
@@ -19,8 +20,8 @@ app.get('/', (req, res) => {
 	res.send('Hello')
 })
 
-app.post('/todos', (req, res) => {
-	const todo = new Todo({ text: req.body.text })
+app.post('/todos', authenticate, (req, res) => {
+	const todo = new Todo({ text: req.body.text, _creator: req.user._id })
 	todo.save().then(doc => res.send(doc), e => res.status(400).send(e))
 })
 
@@ -63,20 +64,39 @@ app.delete('/users/me/token', authenticate, (req, res) => {
 	)
 })
 
-app.get('/todos', (req, res) => {
-	Todo.find().then(todos => res.send({ todos }), e => res.status(400).send(e))
+app.get('/todos', authenticate, (req, res) => {
+	Todo.find({ _creator: req.user._id }).then(
+		todos => res.send({ todos }),
+		e => res.status(400).send(e),
+	)
 })
 
-app.get('/todos/:id', (req, res) => {
+app.get('/todos/:id', authenticate, (req, res) => {
 	let id = req.params.id
 
 	if (!ObjectID.isValid(id)) return res.status(400).send("Id isn't a correct number")
 
-	Todo.findById(id)
+	Todo.findOne({ _id: id, _creator: req.user._id })
 		.then(
-			ret => {
-				if (!ret) res.status(400).send('Todo not find')
-				res.send(ret)
+			todo => {
+				if (!todo) return res.status(400).send('Todo not find')
+				res.send(todo)
+			},
+			e => res.status(400).send('Something wrong : ' + e),
+		)
+		.catch(e => res.status(400).send('Something wrong : ' + e))
+})
+
+app.delete('/todos/:id', authenticate, (req, res) => {
+	let id = req.params.id
+
+	if (!ObjectID.isValid(id)) return res.status(400).send("Id isn't a correct ID")
+
+	Todo.findOneAndRemove({ _id: id, _creator: req.user._id })
+		.then(
+			todo => {
+				if (!todo) return res.status(400).send('Todo not find')
+				res.send(todo)
 			},
 			e => res.status(400).send('Something wrong : ' + e),
 		)
